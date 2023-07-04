a4f44cd8a0ff61a7cebed0e493104ad7edaaebbe
jhonataT <jhonata.tenorio2526@gmail.com>
2023-07-04 19:55:49 -0300

    Adding linear regression path; adding csv file; adding initial python imports

---
+++ linearRegression/index.py
@@ -0,0 +1,12 @@
+# pip install matplotlib
+# pip install pandas
+# pip install -U scikit-learn
+
+from matplotlib import pyplot as plt
+import pandas as pd
+import pylab as np
+from sklearn import linear_model
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
+from math import sqrt
+
